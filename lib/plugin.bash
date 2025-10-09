ef883967b3a2e0f9c3b6e941c948c0207d616d76
Tom Watt <tom@buildkite.com>
2025-10-09 11:37:24 +0100

    feat: add retry func

--- lib/plugin.bash
+++ lib/plugin.bash
@@ -60,3 +60,25 @@ function plugin_read_config() {
   local default="${2:-}"
   echo "${!var:-$default}"
 }
+
+# retry <number-of-retries> <command>
+function retry {
+  local retries=$1; shift
+  local attempts=1
+  local status=0
+
+  until "$@"; do
+    status=$?
+    echo "Exited with $status"
+    if (( retries == "0" )); then
+      return $status
+    elif (( attempts == retries )); then
+      echo "Failed $attempts retries"
+      return $status
+    else
+      echo "Retrying $((retries - attempts)) more times..."
+      attempts=$((attempts + 1))
+      sleep $(((attempts - 2) * 2))
+    fi
+  done
+}
